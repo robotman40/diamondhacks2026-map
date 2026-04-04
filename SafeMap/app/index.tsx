3c0d052f081c4e805a4900e2e693f304edfbe9f4
robotman40 <robotman40@protonmail.com>
2026-04-04 12:05:13 -0700

    Initial login layout

--- SafeMap/app/index.tsx
+++ SafeMap/app/index.tsx
@@ -1,4 +1,35 @@
-import { Text, View } from "react-native";
+import { 
+  Text,
+  View, 
+  TextInput, 
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from 'react';
+
+function LoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <View>
+      <Text >Username</Text>
+      <TextInput
+        value={username}
+        onChangeText={setUsername}
+        placeholder="Enter your username"
+        autoCapitalize="none"
+      />
+
+      <Text>Password</Text>
+      <TextInput
+        value={password}
+        onChangeText={setPassword}
+        placeholder="Enter your password"
+        secureTextEntry
+      />
+    </View>
+  );
+}
 
 export default function Index() {
   return (
@@ -11,6 +42,13 @@ export default function Index() {
     >
       <Text style={{ fontSize: 50, fontWeight: 'bold' }}>SafeRoute</Text>
       <Text>Never worry about routes again!</Text>
+
+      <Text>Username</Text>
+      <TextInput></TextInput>
+
+      <Text>Password</Text>
+      <TextInput></TextInput>
+
     </View>
   );
 }
